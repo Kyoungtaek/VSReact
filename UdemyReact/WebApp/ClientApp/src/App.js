6488ec27557a1ca72ae69d289c19dc30fd424d5d
Kyoungtaek Oh <okts83@gmail.com>
2020-05-23 04:08:44 -0400

    Add first component and link

--- UdemyReact/WebApp/ClientApp/src/App.js
+++ UdemyReact/WebApp/ClientApp/src/App.js
@@ -4,11 +4,13 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import MyCompo from './components/MyCompo';
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  </Layout>
+    <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+        <Route path='/my-compo/' component={MyCompo} />
+    </Layout>
 );
---
+++ UdemyReact/WebApp/ClientApp/src/components/MyCompo.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react'
+
+class MyCompo extends Component {
+    render() {
+        return (
+            //JSX
+            <div>
+                <h1>My First React Component!</h1>
+            </div>
+        );
+    }
+}
+
+export default MyCompo;
